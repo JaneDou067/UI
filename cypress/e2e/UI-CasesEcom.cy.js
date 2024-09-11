5e1944b53094d1c556474103fd1dfc9eeab3807f
Halyna Pyda <halyna_pyda@epam.com>
2024-09-12 01:10:58 +0300

    UI-CasesEcom.cy ongoing work

--- cypress/e2e/UI-CasesEcom.cy.js
+++ cypress/e2e/UI-CasesEcom.cy.js
@@ -21,10 +21,53 @@ describe('Ecom site checks', () => {
         new HomePageEcommerce()
             .openSite()
             .visitHeaderLink('Log in')
-            // .inputRequiredFields()
-            // .submitButtonClick()
-            // .successMessage.contains('Your registration completed')
+            .inputRequiredFields()
+            .submitButtonClick()
+            .logoutButton.should('be.visible')
     });
+
+    it('Verify that ‘Computers’ group has 3 sub-groups with correct names', () => {
+        new HomePageEcommerce()
+            .openSite()
+            .hoverOption()
+            .checkSubMenuOptions(['Desktops','Notebooks','Accessories'])
+    });
+
+    it('Verify that allows sorting items (different options)', () => {
+        new HomePageEcommerce()
+            .openSite()
+            .visitCategoryPage()
+            .checkItemsSorting([
+                ['Name: Z to A', 'orderby=6'],
+                ['Price: Low to High', 'orderby=10'],
+                ['Price: High to Low', 'orderby=11'],
+                ['Created on', 'orderby=15']
+            ]);
+    });
+
+    it('Verify that allows changing number of items on page', () => {
+        new HomePageEcommerce()
+            .openSite()
+            .visitCategoryPage()
+            .checkPageSize([
+                ['4', 'pagesize=4'],
+                ['8', 'pagesize=8'],
+                ['12', 'pagesize=12']
+            ]);
+    });
+
+    it('Verify that allows adding an item to the Wishlist', () => {
+        new HomePageEcommerce()
+            .openSite()
+            .visitCategoryPage()
+            .selectProduct()
+            .addWishlist()
+            .SuccessBanner.should('exist')
+    });
+
+
+
+
 
 
 
--- cypress/support/page_object/HomePageEcommerce.js
+++ cypress/support/page_object/HomePageEcommerce.js
@@ -2,16 +2,32 @@ export default class HomePageEcommerce {
 
     //#region Selectors
     get headerLogo() { return cy.get('.header-logo')}
-    get policiesFooter() { return cy.get('.policies-links-wrapper a')}
-    get regionTile() { return cy.get('.locations-viewer-23__carousel')}
     get headerLink() { return cy.get('.header-links')}
-    get submitButton() { return cy.get('#register-button')}
+    get submitButtonRegister() { return cy.get('#register-button')}
+    get submitButtonLogin() { return cy.get('.login-button')}
     get firstNameField() {return cy.get('#FirstName')}
     get lastNameField() {return cy.get('#LastName')}
     get emailField() {return cy.get('#Email')}
     get passwordField() {return cy.get('#Password')}
     get confirmPasswordField() {return cy.get('#ConfirmPassword')}
     get successMessage() {return cy.get('.result')}
+    get logoutButton() {return cy.get('.ico-logout')}
+    get menuTab(){ return cy.get('.top-menu [href="/computers"]')}
+    get subMenu(){ return cy.get('[href="/computers"] + .top-menu-triangle + .sublist.firstLevel')}
+    get categoryPage(){ return cy.get('.top-menu [href="/digital-downloads"]')}
+    get sorting() {return cy.get('#products-orderby')}
+    get pageSize() {return cy.get('#products-pagesize')}
+    get productPage() {return cy.get('.product-title a[href="/album-3"]')}
+    get addToWishListBtn() {return cy.get('.add-to-wishlist-button')}
+    get SuccessBanner() {return cy.get('.bar-notification.success[style*="display: block"]')}
+
+
+
+
+
+
+
+
 
 
 
@@ -33,74 +49,118 @@ export default class HomePageEcommerce {
         return this;
     }
 
-    inputRequiredFields(){
-        const timestamp = Date.now();
-        const uniqueNames = `Name-${Date.now()}`;
-        const uniqueEmail = `test-${timestamp}@example.com`;
-        const uniquePassword = `Password${timestamp}`;
+    inputRequiredFields() {
+        cy.url().then(url => {
+            if (url.includes('/register')) {
+                const timestamp = Date.now();
+                const uniqueNames = `Name-${timestamp}`;
+                const uniqueEmail = `test-${timestamp}@example.com`;
+                const uniquePassword = `Password${timestamp}`;
 
-        this.firstNameField.type(uniqueNames);
-        this.lastNameField.type(uniqueNames);
-        this.emailField.type(uniqueEmail);
-        this.passwordField.type(uniquePassword);
-        this.confirmPasswordField.type(uniquePassword);
-
-        return this;
-    }
-
-    submitButtonClick(){
-        this.submitButton.click();
-
-        return this;
-    }
-
-
-
-
-    checkElementPresence(selector) {
-        cy.get(selector).should('exist');
-
-        return this;
-    }
-
-
-
-
-
-    checkFooterElements(texts) {
-        texts.forEach(text => {
-            this.policiesFooter.contains(text).should('be.visible');
+                this.firstNameField.type(uniqueNames);
+                this.lastNameField.type(uniqueNames);
+                this.emailField.type(uniqueEmail);
+                this.passwordField.type(uniquePassword);
+                this.confirmPasswordField.type(uniquePassword);
+            } else if (url.includes('/login')) {
+                cy.fixture('profile').then((credentials) => {
+                    this.emailField.type(credentials.email);
+                    this.passwordField.type(credentials.password);
+                });
+            }
         });
 
         return this;
     }
 
-    checkRegionTile(texts) {
-        texts.forEach(text => {
-            this.regionTile.contains(text).should('be.visible');
+    submitButtonClick() {
+        cy.url().then(url => {
+            if (url.includes('/register')) {
+                this.submitButtonRegister.should('be.visible').click();
+            } else if (url.includes('/login')) { // Fixed the regular expression delimiter issue
+                this.submitButtonLogin.should('be.visible').click();
+            }
         });
 
         return this;
     }
 
-
-
-
-
-
-
-
-
-    checkCurrentPage(Url) {
-        cy.url().should('include', Url);
+    hoverOption() {
+        this.menuTab.trigger('mouseover');
 
         return this;
     }
 
-    returnHome() {
-        this.headerLogo.should('be.visible').click()
-         return this;
+    checkSubMenuOptions (texts) {
+        texts.forEach(text => {
+            this.subMenu.contains(text).should('be.visible');
+        });
+
+        return this;
     }
+
+    visitCategoryPage(){
+        this.categoryPage.should('be.visible').click()
+        this.sorting.should('be.visible')
+
+        return this;
+    }
+
+    checkItemsSorting(sortOptions) {
+        sortOptions.forEach(option => {
+            const sortName = option[0];
+            const sortUrlParam = option[1];
+
+            this.sorting.find('option').contains(sortName).should('exist');
+            this.sorting.select(sortName);
+
+            cy.url().should('include', sortUrlParam);
+        });
+
+        return this;
+    }
+
+    checkPageSize(pageSizeOptions) {
+        pageSizeOptions.forEach(option => {
+            const dropdownOption = option[0];
+            const pageSizeUrlParam = option[1];
+
+            this.pageSize.find('option').contains(dropdownOption).should('exist');
+            this.pageSize.select(dropdownOption);
+
+            cy.url().should('include', pageSizeUrlParam);
+        });
+
+        return this;
+    }
+
+    selectProduct() {
+        this.productPage.click()
+
+        return this;
+    }
+
+    addWishlist() {
+        this.addToWishListBtn.click()
+
+        return this;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
