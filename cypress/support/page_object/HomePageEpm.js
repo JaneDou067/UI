803d186abd43c9fbe8f2a66ede3df682def95b07
Natalia_Iaroslavtseva <natalia_iaroslavtseva@epam.com>
2024-09-12 14:52:25 +0300

    review task1 - 12/09/24

--- cypress/support/page_object/HomePageEpm.js
+++ cypress/support/page_object/HomePageEpm.js
@@ -1,19 +1,22 @@
-export default class HomePageEpm {
+export default class HomePageEpm { //todo: you need to get a practice with inheritance Base Page (contains all elements, that are present on all pages), HomePage (class HomePage extends BasePage ),
+    // todo - AboutPage, ContuctUsPage() extends Base Page as well. .clickContactUsButton() will return new ContuctUsPage()
 
     //#region Selectors
-    get modeToggle() { return cy.get('.header__vaulting-container .switch')}
+    get modeToggle() {
+        return cy.get('.header__vaulting-container .switch')
+    }
     get headerLogo() { return cy.get('.header__logo-container')}
     get localeSelector() { return cy.get('.location-selector__button')}
     get policiesFooter() { return cy.get('.policies-links-wrapper a')}
-    get regionTile() { return cy.get('.locations-viewer-23__carousel')}
+    get regionTile() { return cy.get('.locations-viewer-23__carousel')}//todo: did you mean title?
     get regionSwitcher() { return cy.get('.tabs-23__link')}
     get searchIcon() { return cy.get('.header-search-ui')}
     get searchInput() { return cy.get('.header-search__input')}
     get searchSubmit() { return cy.get('.custom-search-button')}
     get headerLink() { return cy.get('[class*="top-navigation__"]')}
-    get contactSubmit() { return cy.get('.button-ui')}
+    get contactSubmit() { return cy.get('.button-ui')} //todo: please type name of element at the end (Button, Link, Field etc)
     get requiredFields() {return cy.get('[aria-required="true"]')}
-    get errorHandledFields() {return cy.get('[aria-invalid="true"]')}
+    get errorHandledFields() {return cy.get('[aria-invalid="true"]')} //todo: it is better to use here "List" - errorHandledFieldList()
     get downloadButton() {return cy.get('[download]')}
 
     //#endregion
@@ -42,13 +45,13 @@ export default class HomePageEpm {
         return this;
     }
 
-    checkElementPresence(selector) {
+    checkElementPresence(selector) { //todo: you
         cy.get(selector).should('exist');
 
         return this;
     }
 
-    chooseLocale(selector) {
+    chooseLocale(selector) { //todo: you can not keep locator as a parameter - it is unclear on your spec what you mean
         this.localeSelector.should('be.visible').click()
         cy.get(selector).should('be.visible').click()
 
@@ -57,22 +60,22 @@ export default class HomePageEpm {
 
     validateCareerSite() {
         cy.origin('https://careers.epam.ua', () => {
-            cy.get('.no-touchevents[lang=\'uk-UA\']').should('exist')
-        });
+            cy.get('.no-touchevents[lang=\'uk-UA\']').should('exist') //todo: you should not keep "should" here, just on spec file. Each your test must be ended with "should" (and can contains more "should" inside it)
+        }); //todo: do not use locator in methods - put it on getter like others locators
         return this;
     }
 
     checkFooterElements(texts) {
         texts.forEach(text => {
-            this.policiesFooter.contains(text).should('be.visible');
+            this.policiesFooter.contains(text).should('be.visible'); //todo: you should not keep "should" her
         });
 
         return this;
     }
 
     checkRegionTile(texts) {
-        texts.forEach(text => {
-            this.regionTile.contains(text).should('be.visible');
+        texts.forEach(text => { //todo: why you used here forEach? in test you checked it 3 times, so I guess it is no need
+            this.regionTile.contains(text).should('be.visible'); //todo: you should not keep "should" her
         });
 
         return this;
